0243538eb69d85bd69493b7db75211e85ce84d59
Robstaine N. S. Macedo <robstaine@gmail.com>
2026-01-21 15:45:13 -0300

    simulando_'environment'_criando_arquivo_'env'_que_mantem_os_dados_sensiveis

--- conexao.php
+++ conexao.php
@@ -1,14 +1,16 @@
 <?php
 
-$host = 'localhost';
+// $host = 'localhost';
 
-$username = 'root';
-$password = '';
-$dbname = '';
-$dsn = "mysql:host=$host;dbname=$dbname";      // string de conexão com banco de dados
+// $username = 'root';
+// $password = '';
+// $dbname = '';
+require_once 'env.php';
+
+$dsn = "mysql:host=$ENV_HOST;dbname=$ENV_DBNAME";      // string de conexão com banco de dados
 
 try {       // captura os erros de conexão
-    $connection = new PDO($dsn, $username, $password);      // fazendo conexão
+    $connection = new PDO($dsn, $ENV_USERNAME, $ENV_PASSWORD);      // fazendo conexão
     echo 'Conexão bem sucedida.';
 } catch (PDOException $e) {
     echo 'Erro na conexão: '. $e-> getMessage();        // método 'getMessage' transforma a string de erro em uma descrição
---
+++ env.php
@@ -0,0 +1,9 @@
+<?php
+
+$ENV_HOST = 'localhost';
+
+$ENV_USERNAME = 'root';
+$ENV_PASSWORD = '';
+$ENV_DBNAME = '';
+
+$ENV_ADMIN_PASSWORD = '';
